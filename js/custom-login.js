4d8173c2f590f2618faf1e84cf2a7ed575f6f628
Giv3Grac3 <yayageace31@gmail.com>
2025-08-15 22:43:00 -0400

    submit button

---
+++ js/custom-login.js
@@ -0,0 +1,29 @@
+let container = document.querySelector(".container");
+// console.log(container);
+
+let form = document.querySelector(".form");
+// console.log(form);
+
+let userName = document.querySelector(".name");
+// console.log(userName);
+
+let userEmail = document.querySelector(".email");
+// console.log(userEmail);
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  console.log(data);
+
+  const dataObj = Object.fromEntries(data.entries());
+  console.log(dataObj);
+
+  let name = dataObj.name;
+  console.log("name:", name);
+
+  console.log(`Welcome, ${name}! You are logged in!`, dataObj);
+  form.reset();
+}
